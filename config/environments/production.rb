f22f3afb37c1f599b821045159b13dc5e6ab9d36
Stephany Ramirez <stephany.ramireze@gmail.com>
2023-03-24 14:13:43 -0700

    change repo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,9 +21,10 @@ Rails.application.configure do
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
 
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? {true}
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
