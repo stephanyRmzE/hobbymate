68e1999f29cc5b2351004d9f6c8459edf2bcdba0
Stephany Ramirez <stephany.ramireze@gmail.com>
2023-03-29 22:46:50 -0700

    render deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,8 +24,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
-
+  config.public_file_server.enabled = true
   Rails.application.config.assets.precompile += %w( *.js ^[^_]*.css *.css.erb )
   # Compress CSS using a preprocessor.
   config.assets.css_compressor = nil
