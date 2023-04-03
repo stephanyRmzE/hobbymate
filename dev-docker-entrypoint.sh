d9042f0b456ffae90da142e63a69f62eb0f106dd
Stephany Ramirez <stephany.ramireze@gmail.com>
2023-04-02 22:55:22 -0700

    docker image db:create

--- dev-docker-entrypoint.sh
+++ dev-docker-entrypoint.sh
@@ -9,6 +9,7 @@ bundle check || bundle install --jobs 20 --retry 5
 
 # Remove pre-existing puma/passenger server.pid
 rm -f $APP_PATH/tmp/pids/server.pid
+rake db:create
 rake db:migrate
 rake db:seed
 # run passed commands
