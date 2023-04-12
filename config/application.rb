01b62aec97f1ae761707b779efa42cfdff5f0249
Stephany Ramirez <stephany.ramireze@gmail.com>
2023-04-12 11:35:12 -0700

    change env hobbymate_database_password

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,8 @@
 require_relative "boot"
 require 'sprockets/railtie'
 require "rails/all"
-
+require 'dotenv'
+Dotenv.load('.env')
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
