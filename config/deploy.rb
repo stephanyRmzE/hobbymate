2aa710bdc8fa6e4a0908b59b0c88d2e915a999db
Stephany Ramirez <stephany.ramireze@gmail.com>
2023-04-12 03:18:11 -0700

    new secret key

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,6 +22,7 @@ set :deploy_to, "/home/stephany/#{fetch :application}"
 
 # Default value for :linked_files is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+append :linked_files, "config/credentials/production.key"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
