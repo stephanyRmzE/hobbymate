c49bdc73b2b58bceacbd3e0959c580d90b15ae73
Stephany Ramirez <stephany.ramireze@gmail.com>
2023-04-17 02:13:19 -0700

    added it a line to see

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,22 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bund
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
- set :keep_releases, 4
+ set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+namespace :deploy do
+  desc 'Create, migrate, and seed the database'
+  task :create_migrate_seed_db do
+    on roles(:db) do |host|
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, 'db:create'
+        end
+      end
+    end
+  end
+end
+
+before 'deploy:assets:precompile', 'deploy:create_migrate_seed_db'
